87bb8e45049783bcf4744338ef10a2520069ed85
Rafiul Hasan Tonmoy <rhtonmoyhd@gmail.com>
2023-02-14 17:59:52 +0600

    Initial commit

---
+++ src/main/java/com/tonmoy/solution/HackerRankProblemSolvingSolutionApplication.java
@@ -0,0 +1,13 @@
+package com.tonmoy.solution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackerRankProblemSolvingSolutionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackerRankProblemSolvingSolutionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tonmoy/solution/HackerRankProblemSolvingSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tonmoy.solution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackerRankProblemSolvingSolutionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
